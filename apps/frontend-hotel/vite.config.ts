ac9ae426f61bbdefdded10f1c1dbc6e62b6bafef
Matthias Falbesoner <mfalbesoner@gmail.com>
2023-09-08 09:24:31 +0200

    change default port

--- apps/frontend-hotel/vite.config.ts
+++ apps/frontend-hotel/vite.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
   cacheDir: '../../node_modules/.vite/apps/frontend-hotel',
 
   server: {
-    port: 4200,
+    port: 4201,
     host: 'localhost',
   },
 
   preview: {
-    port: 4300,
+    port: 4301,
     host: 'localhost',
   },
 
