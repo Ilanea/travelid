48c56136db2b43ec5f8671aa2a469def52085cc9
Matthias Falbesoner <mfalbesoner@gmail.com>
2023-09-13 18:08:31 +0200

    add browser router

--- apps/frontend-hotel/src/routes/index.tsx
+++ apps/frontend-hotel/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { useRoutes } from 'react-router-dom';
 import ExamplePage from '../features/example/pages/Example';
+import ExamplePage2 from '../features/example/pages/Example2';
 
 const publicRoutes = [
   {
