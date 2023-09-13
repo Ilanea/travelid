5a9c2e5d150d9036a92128228fa8ee70ad2c23a0
Matthias Falbesoner <mfalbesoner@gmail.com>
2023-09-13 18:09:09 +0200

    remove old import

--- apps/frontend-hotel/src/routes/index.tsx
+++ apps/frontend-hotel/src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { useRoutes } from 'react-router-dom';
 import ExamplePage from '../features/example/pages/Example';
-import ExamplePage2 from '../features/example/pages/Example2';
 
 const publicRoutes = [
   {
