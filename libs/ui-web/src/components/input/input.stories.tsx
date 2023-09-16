e8811a2c8d3c6e1229e566d0e839ae686b9ab537
Matthias Falbesoner <mfalbesoner@gmail.com>
2023-09-16 09:31:11 +0200

    add storybook

---
+++ libs/ui-web/src/components/input/input.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from './input';
+
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  title: 'Input',
+};
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Primary = {
+  args: {},
+};
+
+export const Example: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const input = canvas.getByRole('textbox');
+
+    await userEvent.type(input, 'Hello, Alpaka Gang!', {
+      delay: 100,
+    });
+  },
+};
