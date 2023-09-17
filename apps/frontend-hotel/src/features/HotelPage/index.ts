856b431183b6c6e8c5925e9c60c717c67e3abf78
Obanzi <heimay237@gmail.com>
2023-09-17 15:05:59 +0200

    Random Tryouts

---
+++ apps/frontend-hotel/src/features/HotelPage/index.ts
@@ -0,0 +1,4 @@
+// hier alle exports aus dem feature
+export * from './pages/HotelPage';
+export * from './pages/Bookings';
+
--- apps/frontend-hotel/src/routes/index.tsx
+++ apps/frontend-hotel/src/routes/index.tsx
@@ -1,15 +1,25 @@
 import { useRoutes } from 'react-router-dom';
 import ExamplePage from '../features/example/pages/Example';
+import HotelPage from "../features/HotelPage/pages/HotelPage";
+import Bookings from "../features/HotelPage/pages/Bookings";
 
 const publicRoutes = [
   {
     path: '/example',
     element: <ExamplePage />,
   },
+  {
+    path: '/HotelPage',
+    element: <HotelPage />,
+  },
+  {
+    path: '/Bookings',
+    element: <Bookings />,
+  },
 ];
 
 export const AppRoutes = () => {
-  const commonRoutes = [{ path: '/', element: <div>Landing Page</div> }];
+  const commonRoutes = [{ path: '/', element: <div>Test</div> }];
 
   const element = useRoutes([...publicRoutes, ...commonRoutes]);
 
