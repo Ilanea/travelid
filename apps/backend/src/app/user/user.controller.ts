397129b187ea75ce56affb6a889d9bd67465936d
Ilanea <Ilanea@users.noreply.github.com>
2023-09-18 19:02:56 +0200

    feat: change password

--- apps/backend/src/app/user/user.controller.ts
+++ apps/backend/src/app/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
-import { EditUserDto } from './dto';
+import { ChangePasswordDto, EditUserDto } from './dto';
 import { UserService } from './user.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
@@ -18,11 +18,19 @@ export class UserController {
     return user;
   }
 
-  @Patch()
+  @Patch('edit')
   editUser(
     @GetUser('id') userId: number,
     @Body() dto: EditUserDto,
   ) {
     return this.userService.editUser(userId, dto);
+  }
+
+  @Patch('password')
+  changePassword(
+    @GetUser('id') userId: number,
+    @Body() dto: ChangePasswordDto,
+  ) {
+    return this.userService.changePassword(userId, dto);
   }
 }
--- apps/backend/src/app/user/user.service.ts
+++ apps/backend/src/app/user/user.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { EditUserDto } from './dto';
+import * as argon from 'argon2';
+import { ChangePasswordDto, EditUserDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -23,4 +24,41 @@ export class UserService {
 
     return user;
   }
+
+  async changePassword(
+    userId: number,
+    dto: ChangePasswordDto,
+  ) {
+    const oldUser = await this.prisma.user.findUnique({
+      where: {id: userId  },
+    });
+    if(oldUser.hash === null) {
+      throw new BadRequestException('User is using Oauth');
+    }
+    if(!await argon.verify(oldUser.hash, dto.oldPassword)) {
+      throw new BadRequestException('Wrong password provided');
+    }
+    if(dto.newPassword === dto.oldPassword) {
+      throw new BadRequestException('New password must be different from old password');
+    }
+    if(dto.newPassword.length < 8) {
+      throw new BadRequestException('New password must be at least 8 characters long');
+    }
+    if(dto.newPassword.length > 32) {
+      throw new BadRequestException('New password must be at most 32 characters long');
+    }
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hash: await argon.hash(dto.newPassword),
+      },
+    });
+
+    delete user.hash;
+
+    return user;
+  }
+
 }
