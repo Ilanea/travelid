7064c76e874df3a1d314d9dd1bb99dd313889dc7
Matthias Falbesoner <mfalbesoner@gmail.com>
2023-09-19 10:53:04 +0200

    feat: Add misc routes

--- apps/frontend-hotel/src/routes/index.tsx
+++ apps/frontend-hotel/src/routes/index.tsx
@@ -1,10 +1,30 @@
 import { useRoutes } from 'react-router-dom';
 import ExamplePage from '../features/example/pages/Example';
+import Signup from '../features/auth/pages/signup';
+import Terms from '../features/misc/pages/terms';
+import Privacy from '../features/misc/pages/privacy';
+import NotFound from '../features/misc/pages/not-found';
 
 const publicRoutes = [
   {
     path: '/example',
     element: <ExamplePage />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/terms',
+    element: <Terms />,
+  },
+  {
+    path: '/privacy',
+    element: <Privacy />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ];
 
