c0c18dd26846700eabfaa7195fb8beb8434e2ca0
Obanzi <heimay237@gmail.com>
2023-09-22 12:16:21 +0200

    Datumsfilter wurde erstellt. Ebenso Radio Buttons für Auswahl ob täglich, monatlich oder jährlich

---
+++ apps/frontend-hotel/src/features/HotelPage/components/BarChart.tsx
@@ -0,0 +1,42 @@
+// BarChartComponent.tsx
+import React from 'react';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
+
+interface BookingsBarChartProps {
+  data: {
+    day: string;
+    Bookings: number;
+  }[];
+}
+
+const BookingsBarChart: React.FC<BookingsBarChartProps> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <XAxis dataKey="day" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="Bookings" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default BookingsBarChart;
