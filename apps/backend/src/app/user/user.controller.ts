6bf2bf9aa6c2fea3e0290e5c20a0949d2ebd05d9
Ilanea <Ilanea@users.noreply.github.com>
2023-09-27 20:03:59 +0200

    feat: avatar upload

--- apps/backend/src/app/user/user.controller.ts
+++ apps/backend/src/app/user/user.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Patch, Query, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, FileTypeValidator, Get, HttpStatus, MaxFileSizeValidator, Param, ParseFilePipe, ParseFilePipeBuilder, Patch, Post, Query, Req, UnauthorizedException, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthenticatedGuard, RolesGuard } from '../auth/guard';
 import { ChangePasswordDto, ChangeRoleDto, EditUserDto, ChangeActiveDto } from './dto';
 import { UserService } from './user.service';
-import { ApiTags, ApiCookieAuth } from '@nestjs/swagger';
+import { ApiTags, ApiCookieAuth, ApiConsumes, ApiBody, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { Roles } from '../auth/decorator';
 import { Role } from '../auth/roles/role.enum';
+import { FileInterceptor } from '@nestjs/platform-express';
+// This is a hack to make Multer available in the Express namespace
+import { Multer, diskStorage } from 'multer';
+import * as fs from 'fs';
+import Path from 'path'
+import path from 'path';
 
 @ApiTags('users')
 @ApiCookieAuth()
@@ -77,6 +83,63 @@ export class UserController {
   async changeActive(@Param('userId') userId: string, @Body() dto: ChangeActiveDto, @Req() request) {
     if (request.session.user.id === parseInt(userId)) {
       const user = await this.userService.changeActive(parseInt(userId), dto);
+      return user;
+    } else {
+      throw new UnauthorizedException('You are not authorized to perform this action.');
+    }
+  }
+
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Avatar',
+    type: 'file',
+    required: true,
+    examples: {
+      file: {
+        value: 'multipart/form-data',
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: (req, file, cb) => {
+        const userId = req.body.userId;
+        const dest = `apps/backend/uploads/${userId}`;
+        fs.access(dest, function (error) {
+          if (error) {
+            return fs.mkdir(dest, (error) => cb(error, dest));
+          } else {
+            return cb(null, dest);
+          }
+        });
+      },
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1e9);
+        cb(null, uniqueSuffix + '_' + file.originalname);
+      },
+    }),
+    limits: { fileSize: 3000000 },
+    fileFilter: (req, file, cb) => {
+      if(req.session.user.id !== parseInt(req.body.userId)) {
+        cb(new UnauthorizedException('You are not authorized to perform this action.'), false);
+      } else { 
+        const acceptedExtensionsList = [".jpg", ".jpeg", ".png"];
+        const extname = path.extname(file.originalname).toLowerCase();
+        if (acceptedExtensionsList.includes(extname)) {
+          cb(null, true);
+        } else {
+          cb(new BadRequestException('File extension not allowed'), false);
+        }
+      }
+    }
+  }))
+  @Post('/:userId/avatar')
+  async uploadProfilePicture(
+    @Param('userId') userId: string,
+    @Req() request,
+    @UploadedFile() file: Express.Multer.File) {
+    if (request.session.user.role === Role.ADMIN || request.session.user.id === parseInt(userId)) {
+      const user = await this.userService.uploadProfilePicture(parseInt(userId), file.filename);
       return user;
     } else {
       throw new UnauthorizedException('You are not authorized to perform this action.');
