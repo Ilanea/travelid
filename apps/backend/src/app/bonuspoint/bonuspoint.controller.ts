4beca4889a79c6cb7be40b9d22160b8bc578c1d8
jansencohen <92267890+jansencohen@users.noreply.github.com>
2023-10-31 21:03:44 +0100

    Update bonuspoint.controller.ts
    several fixes

--- apps/backend/src/app/bonuspoint/bonuspoint.controller.ts
+++ apps/backend/src/app/bonuspoint/bonuspoint.controller.ts
@@ -1,23 +1,18 @@
 import {Controller, Post, Body, Get, Param} from '@nestjs/common';
 import {BonuspointService} from "./bonuspoint.service";
-import {Bonuspoint} from "@prisma/client";
+import {BonusPoint} from "@prisma/client";
 
 @Controller('bonuspoint')
 export class BonuspointController {
     constructor(private readonly bonuspointService: BonuspointService) {}
 
     @Get()
-    async getAllBonuspoints(): Promise<Bonuspoint[]> {
-        return await this.bonuspointService.getAllBonuspoints();
-    }
-
-    @Get(':id')
-    async getBonuspoint(@Param('id') id: string): Promise<Bonuspoint> {
-        return await this.bonuspointService.getBonuspoint(parseInt(id));
-    }
-
-    @Post()
-    async createBonuspoint(@Body() data: Bonuspoint): Promise<Bonuspoint> {
-        return await this.bonuspointService.createBonuspoint(data);
+    async createBonuspoint(@Body() data: BonusPoint): Promise<BonusPoint> {
+        const bonusPointInput: BonusPointCreateInput = {
+            email: data.email,
+            points: data.points,
+            user: { connect: { id: data.userId } }
+        };
+        return await this.bonuspointService.createBonuspoint(bonusPointInput);
     }
 }
