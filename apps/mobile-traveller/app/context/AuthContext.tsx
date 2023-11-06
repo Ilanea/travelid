fb989f3614fcf4004b76ad723847dcebc1b0a377
jansencohen <92267890+jansencohen@users.noreply.github.com>
2023-11-06 18:33:57 +0100

    created context/AuthContext

---
+++ apps/mobile-traveller/app/context/AuthContext.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+import { createContext, useContext, useEffect, useState } from 'react';
+
+interface AuthProps {
+  authState?: { token: string | null; authenticated: boolean | null };
+  onRegister?: (email: string, password: string) => Promise<any>;
+  onLogin?: (email: string, password: string) => Promise<any>;
+  onLogout?: () => Promise<any>;
+}
+
+const TOKEN_KEY = 'my-jwt';
+export const API_URL = 'https://localhost3333.com';
+const AuthContext = createContext<AuthProps>({});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }: any) => {
+  const [authState, setAuthState] = useState<{
+    token: string | null;
+    authenticated: boolean | null;
+  }>({
+    token: null,
+    authenticated: null,
+  });
+
+  const register = async (email: string, password: string) => {
+    try {
+      return await axios.post(`$API_URL}/users`, { email, password });
+    } catch (e) {
+      return { error: true, msg: (e as any).response.data.msg };
+    }
+  };
+
+  const value = {};
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
