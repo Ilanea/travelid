19f05283d9642e2e8f7e6b38c980b59b14ed2bc7
jansencohen <92267890+jansencohen@users.noreply.github.com>
2023-11-12 18:00:50 +0100

    troubleshooting

---
+++ apps/mobile-traveller/app/(tabs)/Home.d.ts
@@ -0,0 +1,4 @@
+declare module '*.ts' {
+  const value: any;
+  export default value;
+}
--- apps/mobile-traveller/app/(tabs)/Home.js
+++ apps/mobile-traveller/app/(tabs)/Home.js
@@ -18,8 +18,8 @@ import {
 } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
-import { UserService } from '../../../backend/src/app/user/user.service.ts';
-import { API_URL } from '../context/AuthContext.tsx';
+import { UserService } from '../../../backend/src/app/user/user.service.js';
+import { API_URL } from '../context/AuthContext.js';
 import { theme } from '../theme/theme.js';
 import { getUserData } from '../utils/apiFunctions.js';
 import {
