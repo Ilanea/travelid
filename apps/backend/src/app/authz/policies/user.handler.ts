9fecc68c6109e8a3fe435530cb77c466f6041392
jansencohen <92267890+jansencohen@users.noreply.github.com>
2023-11-26 13:15:30 +0100

    Update user.handler.ts

--- apps/backend/src/app/authz/policies/user.handler.ts
+++ apps/backend/src/app/authz/policies/user.handler.ts
@@ -1,7 +1,8 @@
-import { IPolicyHandler } from '../decorator/policies-handler.interface';
-import { Action, AppAbility } from '../ability.factory';
 import { subject } from '@casl/ability';
 import { User } from '@prisma/client';
+
+import { Action, AppAbility } from '../ability.factory';
+import { IPolicyHandler } from '../decorator/policies-handler.interface';
 
 const dummyUser: User = {
   id: 0,
@@ -15,34 +16,43 @@ const dummyUser: User = {
   firstName: '',
   lastName: '',
   avatar: '',
+  gender: '',
+  street: '',
+  city: '',
+  country: '',
+  nationality: '',
+  birthday: '',
+  documentNo: '',
+  mobilePhone: '',
+  phone: '',
 };
 
 export class ReadUserHandler implements IPolicyHandler {
   handle(ability: AppAbility, request) {
-    const userId = parseInt(request['params'].userId)
+    const userId = parseInt(request['params'].userId);
 
     dummyUser.id = userId;
-  
+
     return ability.can(Action.Read, subject('User', dummyUser));
   }
 }
 
 export class EditUserHandler implements IPolicyHandler {
   handle(ability: AppAbility, request) {
-    const userId = parseInt(request['params'].userId)
+    const userId = parseInt(request['params'].userId);
 
     dummyUser.id = userId;
-  
+
     return ability.can(Action.Edit, subject('User', dummyUser));
   }
 }
 
 export class ManageUserHandler implements IPolicyHandler {
   handle(ability: AppAbility, request) {
-    const userId = parseInt(request['params'].userId)
+    const userId = parseInt(request['params'].userId);
 
     dummyUser.id = userId;
-  
+
     return ability.can(Action.Manage, subject('User', dummyUser));
   }
 }
