273951afcd1b995ca41c2f400849912db0feb885
jansencohen <92267890+jansencohen@users.noreply.github.com>
2023-11-27 08:09:55 +0100

    Update UserProvider.tsx

--- apps/mobile-test/src/app/provider/UserProvider.tsx
+++ apps/mobile-test/src/app/provider/UserProvider.tsx
@@ -3,36 +3,41 @@ import * as SecureStore from 'expo-secure-store';
 
 import { API_URL } from './AuthProvider';
 
-interface User {
-  userName: string;
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  gender: string;
-  academicDegree: string;
-  street: string;
-  city: string;
-  country: string;
-  nationality: string;
-  birthday: string;
-  documentNo: string;
-  mobilePhone: string;
-  phone: string;
-  bonuspoints: number;
-}
+// interface User {
+//   userName: string;
+//   email: string;
+//   password: string;
+//   firstName: string;
+//   lastName: string;
+//   gender: string;
+//   academicDegree: string;
+//   street: string;
+//   city: string;
+//   country: string;
+//   nationality: string;
+//   birthday: string;
+//   documentNo: string;
+//   mobilePhone: string;
+//   phone: string;
+//   bonuspoints: number;
+// }
 
 export class UserProviderClass {
-  public async setBonuspoints(userId: string, bonuspoints: number) {
+  public setBonuspoints(userId: string, bonuspoints: number) {
+    console.log('setBonuspoints', userId, bonuspoints);
+  }
+
+  public async updateBonuspoints(userId: string, bonuspoints: number) {
     try {
       const result = await axios.post(
         `${API_URL}/api/users/{userId}/bonuspoints`,
         { userId, bonuspoints },
+        // Authorization check is done in the backend
         { withCredentials: true }
       );
 
       // Update the bonuspoints in the local state
-      this.setBonuspoints(bonuspoints);
+      this.setBonuspoints(userId, bonuspoints);
 
       // Update User Info in SecureStore
       await SecureStore.setItemAsync('userInfo', JSON.stringify(result.data));
