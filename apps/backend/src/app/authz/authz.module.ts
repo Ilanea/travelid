68c7d41db213b6cf32bcd931b28cc77a2136f061
Ilanea <Ilanea@users.noreply.github.com>
2023-11-28 12:22:10 +0100

    fix: authz providers

--- apps/backend/src/app/authz/authz.module.ts
+++ apps/backend/src/app/authz/authz.module.ts
@@ -2,10 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { AbilityFactory } from './ability.factory';
 import { EditUserHandler, ManageUserHandler, ReadUserHandler } from './policies/user.handler';
 import { EditHotelHandler, ManageHotelHandler, ReadHotelHandler } from './policies/hotel.handler';
+import { DeleteBookingHandler, EditBookingHandler, ManageBookingHandler, ReadBookingHandler } from './policies/booking.handler';
+import { DeleteReviewHandler, ManageReviewHandler } from './policies/review.handler';
 
 @Global()
 @Module({
-  providers: [AbilityFactory, ReadUserHandler, EditUserHandler, ManageUserHandler, ReadHotelHandler, EditHotelHandler, ManageHotelHandler],
+  providers: [AbilityFactory, ReadUserHandler, EditUserHandler, ManageUserHandler, ReadHotelHandler, EditHotelHandler, ManageHotelHandler, ReadBookingHandler, EditBookingHandler, DeleteBookingHandler, ManageBookingHandler, DeleteReviewHandler, ManageReviewHandler, DeleteReviewHandler],
   exports: [AbilityFactory],
 })
 export class AuthzModule {}
