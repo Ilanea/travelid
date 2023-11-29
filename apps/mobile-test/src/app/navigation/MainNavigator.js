53611f644d36fb011193cf3bebf6e1ae0b61f058
jansencohen <92267890+jansencohen@users.noreply.github.com>
2023-11-29 22:57:49 +0100

    main navigator changed

---
+++ apps/mobile-test/src/app/navigation/MainNavigator.js
@@ -0,0 +1,108 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
+
+import { useAuth } from '../provider/AuthProvider';
+import BonuspunktePage from '../screens/BonuspunktePage';
+import Booking from '../screens/Booking';
+import Home from '../screens/Home';
+import LoadingScreen from '../screens/LoadingScreen';
+import LoginScreen from '../screens/LoginScreen';
+import Profile from '../screens/Profile';
+import RegisterStep1 from '../screens/RegisterStep1';
+import RegisterStep2Optional from '../screens/RegisterStep2Optional';
+import RegisterStep3 from '../screens/RegisterStep3';
+import Results from '../screens/Results';
+import Welcome from '../screens/Welcome';
+
+const Stack = createStackNavigator();
+
+const MainNavigator = () => {
+  const { authState } = useAuth();
+  const [authenticated, setAuthenticated] = useState(authState?.authenticated);
+
+  useEffect(() => {
+    console.log('MainNavigator - authState:', authState);
+    setAuthenticated(authState?.authenticated);
+  }, [authState]);
+
+  if (authState === undefined) {
+    return <LoadingScreen />;
+  }
+
+  return (
+    <Stack.Navigator>
+      {authenticated ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Results"
+            component={Results}
+            options={{
+              headerTransparent: true,
+              headerTitle: 'Innsbruck',
+              headerTitleStyle: {
+                color: 'white',
+                fontSize: 30,
+              },
+            }}
+          />
+          <Stack.Screen
+            name="BonuspunktePage"
+            component={BonuspunktePage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Booking"
+            component={Booking}
+            options={{
+              headerTitle: '',
+              headerTransparent: true,
+              headerStyle: {
+                backgroundColor: 'rgba(84, 106, 131, 0.5)',
+              },
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LoginScreen"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RegisterStep1"
+            component={RegisterStep1}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RegisterStep2Optional"
+            component={RegisterStep2Optional}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RegisterStep3"
+            component={RegisterStep3}
+            options={{ headerShown: false }}
+          />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
+
+export default MainNavigator;
